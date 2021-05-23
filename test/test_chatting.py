b05188e266c4f3f8f9d0c77ea017e3f2ee695c25
zh-hang <zhangh127@163.com>
2021-05-23 15:12:22 +0800

    聊天v0.1完成

--- test/test_chatting.py
+++ test/test_chatting.py
@@ -1,18 +1,17 @@
 import asyncio
 from asyncio.tasks import sleep
 import websockets
+import threading
+import json
+import unittest
 
 async def hello():
     uri = "ws://localhost:2333"
     async with websockets.connect(uri) as websocket:
-        name = input("What's your name? ")
+        msg={"name":"test","msg":"test"}
+        await websocket.send(json.dumps(msg))
+        print(f"> {msg}")
+        res_msg = await websocket.recv()
+        print(json.loads(res_msg))
 
-        await websocket.send(name)
-        print(f"> {name}")
-
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
-
-for i in range(10):
-    asyncio.get_event_loop().run_until_complete(hello())
-    sleep(1000)
+asyncio.get_event_loop().run_until_complete(hello())
