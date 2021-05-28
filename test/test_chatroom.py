904cb8f00e5a2366626f25d3199bc32412c9c237
zh-hang <zhangh127@163.com>
2021-05-28 21:17:36 +0800

    完成jion的测试

--- test/test_chatroom.py
+++ test/test_chatroom.py
@@ -2,12 +2,14 @@ import unittest
 import json
 import requests as re
 
+host='http://124.70.97.253:5000/chatroom'
+
 class TestChatroomMethods(unittest.TestCase):
     def test_test_run(self):
         print('test run successfully')
 
     def test_chatroom_lists(self):
-        url='http://localhost:5000/chatroom/list'
+        url=host+'/list'
         res=re.get(url)
         self.assertIn('room_list',res.text)
         self.assertIn('name',res.text)
@@ -15,23 +17,23 @@ class TestChatroomMethods(unittest.TestCase):
 
     def test_chatroom_join(self):
         wrong_urls=[
-            'http://localhost:5000/chatroom/room?name=you&status=1',
-            'http://localhost:5000/chatroom/room?name',
-            'http://localhost:5000/chatroom/room?status',
-            'http://localhost:5000/chatroom/room?name=YOU&status=9',
-            'http://localhost:5000/chatroom/room'
+            '/room?name=TEST&status=0',
+            '/room?name=you&status=1',
+            '/room?name',
+            '/room?status',
+            '/room?name=YOU&status=9',
+            '/room'
             ]
         for url in wrong_urls:
-            r=re.get(url,'GET')
+            r=re.get(host+url,'GET')
             if(r.status_code==200):
                 self.assertIn(r.json()['res'],['invalid room','invalid status','full room'])
         right_urls=[
-            'http://localhost:5000/chatroom/room?name=YOU&status=1',
-            'http://localhost:5000/chatroom/room?name=YOU&status=0',
-            'http://localhost:5000/chatroom/room?name=TEST&status=0'
+            '/room?name=YOU&status=1',
+            '/room?name=YOU&status=0'
         ]
         for url in right_urls:
-            r=re.get(url,'GET')
+            r=re.get(host+url,'GET')
             self.assertIn(r.json()['res'],['join chatroom','leave chatroom'])
 
 
