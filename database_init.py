40b808639767b34f8bd3c26fd46b24e73acdca88
zh-hang <zhangh127@163.com>
2021-06-13 15:53:06 +0800

    新增五个机器人

--- database_init.py
+++ database_init.py
@@ -15,6 +15,11 @@ c.execute(
     user_password text not null,
     user_avatar_url text);''')
 c.execute('''INSERT INTO user (user_tel,user_name,user_password,user_avatar_url) VALUES (0,'admin','123456','')''')
+c.execute('''INSERT INTO user (user_tel,user_name,user_password,user_avatar_url) VALUES (5101,'robot1','123','')''')
+c.execute('''INSERT INTO user (user_tel,user_name,user_password,user_avatar_url) VALUES (5102,'robot2','123','')''')
+c.execute('''INSERT INTO user (user_tel,user_name,user_password,user_avatar_url) VALUES (5103,'robot3','123','')''')
+c.execute('''INSERT INTO user (user_tel,user_name,user_password,user_avatar_url) VALUES (5104,'robot4','123','')''')
+c.execute('''INSERT INTO user (user_tel,user_name,user_password,user_avatar_url) VALUES (5105,'robot5','123','')''')
 print("table user create successfully")
 conn.commit()
 conn.close()
